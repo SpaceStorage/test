6f0a404ed3d552704f819b62d0e0c886d41543f9
amoshi <amoshi.mandrakeuser@gmail.com>
2021-10-09 08:15:14 +0300

    update prometheus test

--- prometheus/src/main.rs
+++ prometheus/src/main.rs
@@ -1,12 +1,10 @@
 mod metrics;
 
 fn main() {
-    let metricsTree = metrics::prometheus::Prometheus::new();
-    metricsTree::init();
-    ////metrics::prometheus::prometheus_print(r);
+    let mut metrics_tree = metrics::prometheus::Prometheus::new();
 
-    //let mut metrics_tree = metrics::prometheus::prometheus_get_metrics(r.clone());
-    //println!("{}", String::from_utf8(metrics_tree).unwrap());
-    //metrics_tree = metrics::prometheus::prometheus_get_metrics(r);
-    //println!("{}", String::from_utf8(metrics_tree).unwrap());
+    metrics_tree.access.with_label_values(&["myproj", "select"]).inc();
+    metrics_tree.response_time.with_label_values(&["myproj", "select", "0.5"]).set(0.3);
+    let mut metrics_str = metrics_tree.get_metrics();
+    println!("{}", String::from_utf8(metrics_str).unwrap());
 }
--- prometheus/src/metrics/prometheus.rs
+++ prometheus/src/metrics/prometheus.rs
@@ -1,129 +1,62 @@
 //use std::time::Duration;
 //use std::thread;
 
-//use prometheus::{Counter, CounterVec, Encoder, Gauge, GaugeVec, Opts, Registry, TextEncoder};
-use prometheus::{Counter, Encoder, Opts, Registry, TextEncoder, CounterVec};
+use prometheus::{Encoder, Opts, Registry, TextEncoder, CounterVec, GaugeVec};
 
 pub struct Prometheus {
     r: prometheus::Registry,
-    access: prometheus::CounterVec,
+    pub access: prometheus::CounterVec,
+    pub response_time: prometheus::GaugeVec,
 }
 
 impl Prometheus {
-    pub fn new() {
-        let metricTree: Prometheus;
-        metricTree.r = Registry::new();
-        //Some(Registry::new());
-        Some(metricTree);
+    pub fn new() -> Self {
+        let access_opts = Opts::new("spacestorage_access", "access queries");
+        let response_time_opts = Opts::new("spacestorage_response_time", "response time");
+        let metric_tree = Prometheus {
+            r: Registry::new(),
+            access: CounterVec::new(access_opts, &["project", "operation"]).unwrap(),
+            response_time: GaugeVec::new(response_time_opts, &["project", "operation", "quantile"]).unwrap(),
+        };
+
+        metric_tree.r.register(Box::new(metric_tree.access.clone())).unwrap();
+        metric_tree.r.register(Box::new(metric_tree.response_time.clone())).unwrap();
+
+        return metric_tree;
     }
-//
-//    pub fn create(&self) {
-//        let counter_opts = Opts::new("test_counter", "test counter help")
-//            .const_label("a", "1")
-//            .const_label("b", "2");
-//        let counter = Counter::with_opts(counter_opts).unwrap();
-//        self.r.register(Box::new(counter.clone())).unwrap();
-//    }
 
-//pub fn init(&self) -> prometheus::Registry {
-pub fn init(&self) {
-    //let r = Registry::new();
-    //let metricsTree = Prometheus::new();
-    //let r = metricsTree.r;
+    ////pub fn init(&self) -> prometheus::Registry {
+    //pub fn init(&mut self) {
+    //    //thread::spawn(move || {
+    //    //    for _ in 0..10 {
+    //    //        thread::sleep(Duration::from_millis(500));
+    //    //        c2.inc();
+    //    //        cv2.with_label_values(&["3", "4"]).inc();
+    //    //        g2.inc();
+    //    //        gv2.with_label_values(&["3", "4"]).inc();
+    //    //    }
+    //    //});
+    //
+    //    //thread::spawn(move || {
+    //    //    for _ in 0..5 {
+    //    //        thread::sleep(Duration::from_secs(1));
+    //    //        counter.inc();
+    //    //        counter_vec.with_label_values(&["3", "4"]).inc();
+    //    //        gauge.dec();
+    //    //        gauge_vec.with_label_values(&["3", "4"]).set(42.0);
+    //    //    }
+    //    //});
+    //}
 
-    let counter_opts = Opts::new("test_counter", "test counter help")
-        .const_label("a", "1")
-        .const_label("b", "2");
-    let counter = Counter::with_opts(counter_opts).unwrap();
-    let counter_vec_opts = Opts::new("test_counter_vec", "test counter vector help")
-        .const_label("a", "1")
-        .const_label("b", "2");
-    let counter_vec = CounterVec::new(counter_vec_opts, &["c", "d"]).unwrap();
+    pub fn get_metrics(&mut self) -> std::vec::Vec::<u8> {
+        let mut buffer = Vec::<u8>::new();
+        let encoder = TextEncoder::new();
+        let metric_families = self.r.gather();
+        encoder.encode(&metric_families, &mut buffer).unwrap();
 
-    self.r.register(Box::new(counter.clone())).unwrap();
-    self.r.register(Box::new(counter_vec.clone())).unwrap();
-
-    //let gauge_opts = Opts::new("test_gauge", "test gauge help")
-    //    .const_label("a", "1")
-    //    .const_label("b", "2");
-    //let gauge = Gauge::with_opts(gauge_opts).unwrap();
-    //let gauge_vec_opts = Opts::new("test_gauge_vec", "test gauge vector help")
-    //    .const_label("a", "1")
-    //    .const_label("b", "2");
-    //let gauge_vec = GaugeVec::new(gauge_vec_opts, &["c", "d"]).unwrap();
-
-    //r.register(Box::new(gauge.clone())).unwrap();
-    //r.register(Box::new(gauge_vec.clone())).unwrap();
-
-    counter.inc();
-    counter.inc_by(42.0);
-
-    counter_vec.with_label_values(&["3", "4"]).inc();
-
-    counter_vec.with_label_values(&["8", "109"]).inc_by(42.0);
-
-    //gauge.inc();
-    //gauge.add(42.0);
-
-    //gauge_vec.with_label_values(&["3", "4"]).inc();
-
-    //gauge_vec.with_label_values(&["3", "4"]).set(42.0);
-
-    //let c2 = counter.clone();
-    //let cv2 = counter_vec.clone();
-    //let g2 = gauge.clone();
-    //let gv2 = gauge_vec.clone();
-    //thread::spawn(move || {
-    //    for _ in 0..10 {
-    //        thread::sleep(Duration::from_millis(500));
-    //        c2.inc();
-    //        cv2.with_label_values(&["3", "4"]).inc();
-    //        g2.inc();
-    //        gv2.with_label_values(&["3", "4"]).inc();
-    //    }
-    //});
-
-    //thread::spawn(move || {
-    //    for _ in 0..5 {
-    //        thread::sleep(Duration::from_secs(1));
-    //        counter.inc();
-    //        counter_vec.with_label_values(&["3", "4"]).inc();
-    //        gauge.dec();
-    //        gauge_vec.with_label_values(&["3", "4"]).set(42.0);
-    //    }
-    //});
-
-    let counter_vec_opts = Opts::new("spacestorage_access", "access queries");
-    self.access = CounterVec::new(counter_vec_opts, &["project"]).unwrap();
-    self.r.register(Box::new(self.access.clone())).unwrap();
-    self.access.with_label_values(&["3", "4"]).inc();
-}
-
-////pub fn prometheus_print(r: prometheus::Registry) {
-////    // Choose your writer and encoder.
-////    let mut buffer = Vec::<u8>::new();
-////    let encoder = TextEncoder::new();
-////    for _ in 0..5 {
-////        let metric_families = r.gather();
-////        encoder.encode(&metric_families, &mut buffer).unwrap();
-////
-////        // Output to the standard output.
-////        println!("{}", String::from_utf8(buffer.clone()).unwrap());
-////
-////        buffer.clear();
-////        thread::sleep(Duration::from_secs(1));
-////    }
-////}
-}
-
-pub fn prometheus_get_metrics(r: prometheus::Registry) -> std::vec::Vec::<u8> {
-    let mut buffer = Vec::<u8>::new();
-    let encoder = TextEncoder::new();
-    let metric_families = r.gather();
-    encoder.encode(&metric_families, &mut buffer).unwrap();
-
-    // Output to the standard output.
-    //println!("{}", String::from_utf8(buffer.clone()).unwrap());
-    //buffer.clear();
-    return buffer;
+        // Output to the standard output.
+        //println!("{}", String::from_utf8(buffer.clone()).unwrap());
+        //buffer.clear();
+        return buffer;
+    }
 }
