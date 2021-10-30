188fa70000b89e538d00327c1d2b547abcc35ce5
amoshi <amoshi.mandrakeuser@gmail.com>
2021-10-30 12:20:38 +0300

    server add support tls, wile write

--- server3/src/fs/write.rs
+++ server3/src/fs/write.rs
@@ -1,6 +1,14 @@
 extern crate tokio;
 use tokio::fs;
+use tokio::io::AsyncWriteExt;
 
 pub async fn write_bytes(bytes: &[u8]) {
-    fs::write("/var/log/test.log", &bytes).await.unwrap();
+    //fs::append("/var/log/test.log", &bytes).await.unwrap();
+    let mut file = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("/var/log/test.log")
+        .await
+        .unwrap();
+     file.write_all(&bytes).await.unwrap();
 }
