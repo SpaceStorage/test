19e90f221e9068b5eb002af786e648728afb28c7
amoshi <amoshi.mandrakeuser@gmail.com>
2021-10-30 13:16:04 +0300

    server add support tls, wile write

--- server3/src/handler/call/interface.rs
+++ server3/src/handler/call/interface.rs
@@ -1,11 +1,19 @@
 use std::pin::Pin;
 use futures::future::join_all;
+//use bstr;
+
 #[path = "../../fs/write.rs"] mod write;
 
-pub async fn run(data: &Vec<u8>) -> &[u8] {
+pub async fn run(data: &[u8]) -> &[u8] {
+
+    let newline : &[u8] = &[0x0a];
+    let res:Vec<u8> = [data, newline].concat();
+    let write_op = write::write_bytes(&res);
+    //let res = bstr::concat([data, &[0x0a]]);
+    //let res_bytes = &res;
+    //let write_op = write::write_bytes(res_bytes);
 
     let mut fut: Vec<Pin<Box<dyn warp::Future<Output = ()>>>> = Vec::new();
-    let write_op = write::write_bytes(data);
     fut.push(Box::pin(write_op));
 
     join_all(fut).await;
