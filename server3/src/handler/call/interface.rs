b504a4dc4730818edbd367a7ae9ee026aac78be2
amoshi <amoshi.mandrakeuser@gmail.com>
2021-11-02 09:51:07 +0300

    test 2

--- server3/src/handler/call/interface.rs
+++ server3/src/handler/call/interface.rs
@@ -4,30 +4,38 @@ use futures::future::join_all;
 #[path = "../../util/global.rs"] mod global;
 use global::GLOBAL;
 
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 pub async fn run(data: &[u8]) -> &[u8] {
     let newline : &[u8] = &[0x0a];
     let res:Vec<u8> = [data, newline].concat();
 
     let mut hm = GLOBAL.lock().unwrap();
-    let buf = hm.buffer.get("1");
+    let buf = hm.buffer.get_mut("1");
     if buf == Option::None {
+        println!("res len is {}", res.len());
         hm.insert("1".to_string(), res.clone());
         return "ok".as_bytes();
     }
 
-    let data = buf.unwrap();
+    let bufdata = buf.unwrap();
+    //println!("bufdata is {:?}", bufdata);
+    print_type_of(&bufdata);
 
-    println!("data len is {}", data.len());
-    if data.len() >= hm.buffer_size {
-        println!("data len is {}, write!", data.len());
-        let write_op = write::write_bytes(&data);
+    println!("bufdata len is {}, capacity is {}", bufdata.len(), bufdata.capacity());
+    if bufdata.len() >= 100000 {
+        println!("bufdata len is {}, write!", bufdata.len());
+        let write_op = write::write_bytes(&bufdata);
         let mut fut: Vec<Pin<Box<dyn warp::Future<Output = ()>>>> = Vec::new();
         fut.push(Box::pin(write_op));
 
         join_all(fut).await;
         hm.buffer.clear();
     } else {
-        hm.insert("1".to_string(), res.clone());
+        //hm.insert("1".to_string(), res.clone());
+        bufdata.extend(res.clone());
     }
 
 
