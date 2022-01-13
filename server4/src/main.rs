c23189d4ccc8836cb3166fd1aab32fdc48cd8458
amoshi <amoshi.mandrakeuser@gmail.com>
2022-01-13 18:15:32 +0300

    update prometheus multi-threaded

--- server4/src/main.rs
+++ server4/src/main.rs
@@ -195,7 +195,7 @@ async fn udp_server_start(rt: &Runtime, addr: &str, size: usize) {
             rt.spawn(async move {
                 //let ten_millis = Duration::from_millis(1000);
                 //thread::sleep(ten_millis);
-                let x = pi(14);
+                let x = pi(13);
                 println!("x is {}", x);
                 println!("spawned thread has id {}", thread_id::get());
                 METRIC_ACCESS_COUNTER.with_label_values(&["global", "global", "udp"]).inc();
