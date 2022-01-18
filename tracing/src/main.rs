80909a959e65a0af498471ba2bc5897b8f754f26
amoshi <amoshi.mandrakeuser@gmail.com>
2022-01-18 08:55:37 +0300

    test tracing to file

--- tracing/src/main.rs
+++ tracing/src/main.rs
@@ -1,18 +1,35 @@
+use tracing::{info, Level};
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
+use tracing_subscriber;
 
 fn main() {
+
+    // stdout logger
+    let collector = tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    // file logger
     let out_file = RollingFileAppender::new(Rotation::NEVER, "./logs", "debug.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(out_file);
+    let (file_logger, _guard) = tracing_appender::non_blocking(out_file);
+    let file_outer = tracing_subscriber::fmt()
+            .with_writer(file_logger)
+            .with_ansi(true)
+            .json()
+            .with_max_level(tracing::Level::TRACE)
+            .finish();
 
-    tracing_subscriber::fmt()
-        .with_writer(non_blocking)
-        .with_ansi(false)
-        .with_max_level(tracing::Level::TRACE)
-        .init();
+    // log to stdout
+    tracing::subscriber::with_default(collector, || {
+        info!("This will be logged to stdout");
+    });
 
-    tracing::info!("sleeping for a minute...");
+    // log to file
+    tracing::subscriber::with_default(file_outer, || {
+        info!("This will be logged to file");
+    });
 
-    std::thread::sleep(std::time::Duration::from_secs(1));
-
-    tracing::info!("bye!");
+    // log to devnull
+    info!("This will _not_ be logged to stdout");
 }
+
