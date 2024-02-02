55817ab5071962b7df87d552a790714172299dc6
amoshi <amoshi.mandrakeuser@gmail.com>
2024-02-02 14:00:19 +0300

    commit with bug as feature

--- server3/src/handler/mod.rs
+++ server3/src/handler/mod.rs
@@ -1,6 +1,6 @@
 use futures::future::BoxFuture;
 use futures::FutureExt;
-//use futures::Future;
+use std::sync::Arc;
 pub mod call;
 pub mod redis;
 
@@ -13,21 +13,13 @@ async fn aw(data: &[u8]) -> String {
     "check_ok".to_string()
 }
 
-type AsyncFn = Box<dyn Fn(&[u8]) -> BoxFuture<String>>;
+type AsyncFn = Box<dyn Fn(&[u8]) -> BoxFuture<String> + Send + Sync>;
 
-pub fn get_handlers() -> Vec::<AsyncFn> {
-    //let funpointer: Vec<Box<dyn Fn(&[u8]) -> String>> = vec![Box::new(redis::interface::run), Box::new(w)];
-    //let funpointer2: Vec<Fn(&[u8]) -> String> = vec![redis::interface::run, Box::new(w)];
-    //let mut fut: Vec<Box<dyn std::future::Future<Output = ()>>> = Vec::new();
-    //fut.push(Box::new(redis::interface::run));
-
-    //funpointer[1]("check1".as_bytes());
-    //funpointer[0]("check2".as_bytes());
-
+pub fn get_handlers() -> Vec::<Arc<AsyncFn>> {
     //return functions
-    let mut funpointer = Vec::<AsyncFn>::new();
-    funpointer.push(Box::new(|data| stub(data).boxed()));
-    funpointer.push(Box::new(|data| redis::interface::run(data).boxed()));
+    let mut funpointer = Vec::<Arc<AsyncFn>>::new();
+    funpointer.push(Arc::new(Box::new(|data| stub(data).boxed())));
+    funpointer.push(Arc::new(Box::new(|data| redis::interface::run(data).boxed())));
 
     return funpointer
 }
